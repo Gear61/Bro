48e3f71508ab11eb1c421f7c1f0d692063339781
Gear61 <chessnone@yahoo.com>
2015-11-03 18:23:19 -0800

    Swapping out Toast for Snackbar

--- app/src/main/java/randomappsinc/com/bro/MainActivity.java
+++ app/src/main/java/randomappsinc/com/bro/MainActivity.java
@@ -1,6 +1,8 @@
 package randomappsinc.com.bro;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
 import android.text.Editable;
@@ -10,12 +12,13 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.TextView;
 
+import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 
 import butterknife.Bind;
-import butterknife.BindString;
+import butterknife.BindColor;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.OnItemClick;
@@ -24,10 +27,9 @@ import butterknife.OnTextChanged;
 public class MainActivity extends AppCompatActivity {
     @Bind(R.id.search_input) EditText searchInput;
     @Bind(R.id.friends) ListView friends;
-
-    @BindString(R.string.confirm_bro) String confirmBro;
-    @BindString(R.string.yes) String yes;
-    @BindString(R.string.no) String no;
+    @Bind(R.id.coordinator_layout) View parent;
+    @BindColor(R.color.black) int black;
+    @BindColor(R.color.white) int white;
 
     private FriendsAdapter friendsAdapter;
 
@@ -58,24 +60,28 @@ public class MainActivity extends AppCompatActivity {
 
     public void showConfirmationDialog(final Friend friend) {
         new MaterialDialog.Builder(this)
-                .title(confirmBro)
+                .title(R.string.confirm_bro)
                 .content("Are you sure you want to Bro " + friend.getFriendName() + "?")
-                .positiveText(yes)
-                .negativeText(no)
-                .callback(new MaterialDialog.ButtonCallback() {
+                .positiveText(android.R.string.yes)
+                .negativeText(android.R.string.no)
+                .onPositive(new MaterialDialog.SingleButtonCallback() {
                     @Override
-                    public void onPositive(MaterialDialog dialog) {
-                        super.onPositive(dialog);
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
                         SmsManager.getDefault().sendTextMessage(friend.getPhoneNumber(), null, "Bro", null, null);
-                        Toast.makeText(MyApplication.getAppContext(), "You bro-ed " + friend.getFriendName() + ".", Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onNegative(MaterialDialog dialog) {
-                        super.onNegative(dialog);
+                        showBroSnackbar(parent, friend, black, white);
                     }
                 })
                 .show();
+    }
+
+    public static void showBroSnackbar(View parent, Friend friend, int backgroundColor, int textColor) {
+        String message = "You bro-ed " + friend.getFriendName() + ".";
+        Snackbar snackbar = Snackbar.make(parent, message, Snackbar.LENGTH_LONG);
+        View view = snackbar.getView();
+        view.setBackgroundColor(backgroundColor);
+        TextView tv = (TextView) view.findViewById(android.support.design.R.id.snackbar_text);
+        tv.setTextColor(textColor);
+        snackbar.show();
     }
 
     @Override
